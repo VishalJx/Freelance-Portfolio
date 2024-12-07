304b22e21c2f2a3cf9f0c43d652d95bcd8e963e8
VishalJx <itsmevishal360@gmail.com>
2024-12-07 14:48:27 +0530

    added profile photo

--- components/IntroCard.jsx
+++ components/IntroCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { FaInstagram } from "react-icons/fa";
 import { FiYoutube } from "react-icons/fi";
 import { LuTwitter } from "react-icons/lu";
-
+import { PiLinkedinLogoBold } from "react-icons/pi";
 
 
 const IntroCard = () => {
@@ -11,29 +11,35 @@ const IntroCard = () => {
     const socials = [
         {
             id: 1,
+            name: 'Linkedin',
+            icon: <PiLinkedinLogoBold />,
+            link: 'https://www.linkedin.com/in/priyapal/'
+        },
+        {
+            id: 2,
             name: 'Instagram',
             icon: <FaInstagram />,
             link: 'https://www.instagram.com/priyapal/'
         },
         {
-            id: 2,
+            id: 3,
             name: 'Youtube',
             icon: <FiYoutube />,
             link: 'https://www.youtube.com/priyapal/'
         },
         {
-            id: 3,
+            id: 4,
             name: 'Twitter',
             icon: <LuTwitter />,
             link: 'https://www.twitter.com/priyapal/'
-        }
+        },
     ]
 
   return (
     <div className='lg:w-[22rem] w-[80vw] mx-auto lg:mx-0 h-[60vh] lg:h-[68vh] flex flex-col items-center justify-center gap-3 border rounded-2xl bg-[#fff6f6] text-black lg:fixed relative'>
         <div className='w-[16rem] lg:w-[15rem]'>
             <Image
-                src="/assets/priya.png"
+                src="/assets/priya.jpg"
                 alt="PRIYA"
                 width={250}
                 height={280}
@@ -45,9 +51,9 @@ const IntroCard = () => {
         <p className='font-kanit w-[60%] lg:w-[65%] text-[1.1rem] text-textGray font-semibold text-center mt-3'>
             A Software Engineer who has developed countless innovative solutions.
         </p>
-        <div className='text-2xl flex gap-2 items-center mt-3'>
+        <div className='text-2xl flex gap-8 items-center mt-3'>
             {socials.map(social => (
-                <a key={social.id} href={social.link} className='flex items-center text-orange px-3 py-2 rounded-lg'>
+                <a key={social.id} href={social.link} className='flex items-center p-1 text-orange rounded-lg hover:bg-orange hover:text-white transition-all duration-300'>
                     {social.icon}
                 </a>
             ))}
